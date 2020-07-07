20dfc1cb2ebb4a16e80f3a7cf7e6f015f9ca14e6
flextremedev <p.wegner1992@gmail.com>
2020-07-07 19:48:23 +0200

    Add envs and parserOptions to eslint-config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,5 +1,10 @@
 module.exports = {
+  env: { browser: true, jest: true, node: true },
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
   rules: {
     "no-console": "error",
   },
