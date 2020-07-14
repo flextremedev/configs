1cb85eb04d81f2a4bbb43a3480cac6d99b605901
flextremedev <p.wegner1992@gmail.com>
2020-07-14 19:35:47 +0200

    Add parser to eslint-config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,11 +1,13 @@
 module.exports = {
   env: { browser: true, jest: true, node: true },
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parser: "babel-eslint",
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2020,
   },
   rules: {
     "no-console": "error",
+    "no-implicit-coercion": "error",
   },
 };
