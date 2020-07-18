e40402929c6c83ff21e911d9d529be32e90212b2
flextremedev <p.wegner1992@gmail.com>
2020-07-18 22:18:36 +0200

    Add eslint-config-typescript config

---
+++ packages/eslint-config-typescript/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    extends: ["@flextremedev/eslint-config", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+};
