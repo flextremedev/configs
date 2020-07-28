d36607d5bf64aaee4d83e84ecdc7444b98332ee1
flextremedev <p.wegner1992@gmail.com>
2020-07-28 20:34:40 +0200

    Add typescript rules

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,5 +1,17 @@
 module.exports = {
-    extends: ["@flextremedev/eslint-config", "plugin:@typescript-eslint/recommended"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
+  extends: [
+    "@flextremedev/eslint-config",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      { allowTypedFunctionExpressions: true },
+    ],
+    "@typescript-eslint/prefer-interface": "off",
+  },
 };
