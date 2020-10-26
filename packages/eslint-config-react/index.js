a36574c1f7dc4afee14697fdef39a911c9cde867
flextremedev <p.wegner1992@gmail.com>
2020-10-26 22:39:32 +0100

    Turn off react in jsx scope rule

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -7,6 +7,7 @@ module.exports = {
   rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
+    "react/react-in-jsx-scope": "off",
     "import/order": [
       "error",
       {
