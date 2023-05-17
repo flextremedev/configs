5327ec7c98cff87f8dfcf3540fc1c19256e207d8
Pascal Wegner <pascal.wegner@edith.care>
2023-05-17 17:07:08 +0200

    Use typescript eslint only on ts(x) files

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,14 +1,22 @@
 module.exports = {
-  extends: [
-    "@flextremedev/eslint-config",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/typescript",
-  ],
+  extends: ["@flextremedev/eslint-config"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  rules: {
-    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-    "@typescript-eslint/prefer-interface": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-  },
+  overrides: [
+    {
+      files: ["*.ts?(x)"],
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+        "@typescript-eslint/explicit-function-return-type": [
+          "error",
+          { allowTypedFunctionExpressions: true },
+        ],
+        "@typescript-eslint/prefer-interface": "off",
+      },
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+      ],
+    },
+  ],
 };
